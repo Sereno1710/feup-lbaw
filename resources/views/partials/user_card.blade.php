9a3d45b9ffedafbaed4af70d67473b0f19a86d29
Máximo <up202108887@up.pt>
2023-11-22 18:53:17 +0000

    added links in user search result

--- resources/views/partials/user_card.blade.php
+++ resources/views/partials/user_card.blade.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Str;
 $bio=Str::limit($user->bio, 30);
 ?>
 
-<a href="{{ url('/users/1') }}">
+<a href="{{ url('/user/' . $user->id) }}">
     <div class="bg-white text-stone-800 p-2 rounded-lg shadow-lg flex flex-col items-center">
         <h4 class="font-bold text-xl">{{ $user->name }}</h4>
         <p class="text-gray-500">&#64;{{ $user->username }}</p>

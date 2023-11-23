84ee1edf4c793f7500126cf70fe5c0442b461695
Daniel Gago <danieljagago@gmail.com>
2023-11-23 01:21:20 +0000

    Added ajax to admin user page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <title>SoundSello</title>
     @vite('resources/css/app.css')
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/admin.js') }}" defer></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>

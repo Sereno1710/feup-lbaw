29d90ef7e1f948b3e0b3183669d14588f468824e
Eduardo Oliveira <up202108843@up.pt>
2023-12-07 18:54:52 +0000

    Added Home Page Photo

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
                 <h2>Welcome to SoundSello.<br>Where bidding hits<br>all the right notes.</h2>
             </div>
             <div class="image">
-                <img src="https://cdn.discordapp.com/attachments/806919335356006451/1177017030080204871/30-musicas-de-superacao-que-vao-inspirar-voce.png?ex=6570f987&is=655e8487&hm=431d300755fb9c23b75b85af91304ac98863df7e07ab8f1bc2438f0f3e299494&" alt="Home page photo">
+                <img src="images/home/home.jpg" alt="Home page photo">
             </div>
         </div>
 

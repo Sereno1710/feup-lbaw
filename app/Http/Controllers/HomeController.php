1a2b09728e4300a4c146fc90a444859e79b2aebd
Máximo <up202108887@up.pt>
2023-12-12 19:46:51 +0000

    exact match search shows up first

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,19 +36,32 @@ class HomeController extends Controller
             // split on 1+ whitespace & ignore empty (eg. trailing space)
             $searchValues = preg_split('/\s+/', $input, -1, PREG_SPLIT_NO_EMPTY);
 
-            $auctionsQuery = Auction::where(function ($q) use ($searchValues) {
+            $auctionsQuery1 = Auction::where('name', 'like', $input)
+                ->orWhere('category', 'like', $input)
+                ->orWhere('description', 'like', $input)
+                ->get();
+
+            $usersQuery1 = User::where('name', 'like', $input)
+                ->orWhere('username', 'like', $input)
+                ->orWhere('biography', 'like', $input)
+                ->get();
+
+            $auctionsQuery2 = Auction::where(function ($q) use ($searchValues) {
                 foreach ($searchValues as $value) {
                     $q->orWhereRaw("tsvectors @@ to_tsquery('english', ?)", [$value . ':*'])
                         ->orderByRaw("ts_rank(tsvectors, to_tsquery('english', ?)) DESC", [$value . ':*']);
                 }
             })->get();
 
-            $usersQuery = User::where(function ($q) use ($searchValues) {
+            $usersQuery2 = User::where(function ($q) use ($searchValues) {
                 foreach ($searchValues as $value) {
                     $q->orWhereRaw("tsvectors @@ to_tsquery('english', ?)", [$value . ':*'])
                         ->orderByRaw("ts_rank(tsvectors, to_tsquery('english', ?)) DESC", [$value . ':*']);
                 }
             })->get();
+
+            $auctionsQuery = $auctionsQuery1->merge($auctionsQuery2);
+            $usersQuery = $usersQuery1->merge($usersQuery2);
         }
 
         // Add a type property to distinguish between users and auctions

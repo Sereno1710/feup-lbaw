643ed793e037f5410c2d2f769f215f5062c53e72
Máximo <up202108887@up.pt>
2023-12-19 23:45:20 +0000

    fixed no search results found condition

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -5,6 +5,10 @@
 <div class="container mx-auto my-8">
     <h1 class="text-3xl font-bold mb-6">Search Results</h1>
 
+    @if($results->isEmpty())
+    <p class="text-gray-600">No results found.</p>
+    @else
+
     <div class="flex space-x-4" id="filters">
         <button
             class="button all bg-white border-black text-black hover:bg-gray-200 font-bold py-2 px-4 rounded-full border"
@@ -18,9 +22,6 @@
     </div>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8" id="results">
-        @if($results->isEmpty())
-        <p class="text-gray-600">No results found.</p>
-        @else
         @foreach($results as $result)
         @if($result->type === 'auction')
         <div class="auction">
@@ -32,8 +33,8 @@
         </div>
         @endif
         @endforeach
-        @endif
     </div>
+    @endif
 </div>
 {{ $results->appends(['input' => $input])->links() }}
 <script>
